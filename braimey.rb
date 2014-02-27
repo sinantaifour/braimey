bb23cb266887fd4f354a14dd686e3082f92480f1
Sinan Taifour <sinan@taifour.com>
2014-02-27 02:19:02 +0100

    Support litecoin addresses too.

--- braimey.rb
+++ braimey.rb
@@ -8,7 +8,8 @@ require 'digest'
 require 'io/console'
 
 def hex_private_key_to_wallet_import_format(priv)
-  extpriv = ["80" + priv].pack("H*")
+  version = { :litecoin => "B0", :bitcoin => "80" }[PROTOCOL] || "80"
+  extpriv = [version + priv].pack("H*")
   csm = Digest::SHA256.digest(Digest::SHA256.digest(extpriv))[0..3]
   Base58.encode58(extpriv + csm)
 end
@@ -17,7 +18,8 @@ def hex_public_key_to_wallet_import_format(pub)
   intermediate = [pub].pack("H*")
   intermediate = Digest::SHA256.digest(intermediate)
   intermediate = Digest::RMD160.digest(intermediate)
-  intermediate = "\x00" + intermediate
+  version = { :litecoin => "0", :bitcoin => "\x00" }[PROTOCOL] || "\x00"
+  intermediate = version + intermediate
   extended = intermediate
   intermediate = Digest::SHA256.digest(intermediate)
   intermediate = Digest::SHA256.digest(intermediate)
@@ -35,6 +37,15 @@ def hex_private_key_to_hex_public_key(priv)
   point = ECDSA::Point.new(p, a, b, gx, gy, r)
   res = point * priv.to_i(16)
   "04" + ("0" * 64 + res.x.to_s(16))[-64..-1]+ ("0" * 64 + res.y.to_s(16))[-64..-1]
+end
+
+# === Read the input. ===
+
+if ARGV[0] == "-litecoin"
+  PROTOCOL = :litecoin
+  ARGV.shift
+else
+  PROTOCOL = :bitcoin
 end
 
 seed = nil
@@ -57,8 +68,10 @@ unless seed
   exit(1)
 end
 
+# === Generate keys and output. ===
+
 priv = Digest::SHA256.hexdigest(seed)
 pub = hex_private_key_to_hex_public_key(priv)
-# puts %Q{Seed: "#{seed}".}
+puts "\e[34mAddresses for the #{PROTOCOL.capitalize} network.\e[0m"
 puts %Q{Hash: #{priv}.}
 puts %Q{Address Pair: #{hex_public_key_to_wallet_import_format(pub)}:#{hex_private_key_to_wallet_import_format(priv)}.}
