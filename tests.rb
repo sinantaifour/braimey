5833bb6816a42412b4c96d0d6ab6472683829d1b
Sinan Taifour <sinan@taifour.com>
2017-03-15 10:11:02 +0100

    Added basic test.

---
+++ tests.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+
+def run(protocol, result)
+  print "Testing #{protocol} ... "
+  if `./braimey.rb -#{protocol} -s "thisisatest"`.split("\n").grep(/Address Pair/).first.split(": ").last == result + "."
+    puts "OK."
+  else
+    puts "FAILED!"
+  end
+end
+
+run("bitcoin", "1GLyv3DuCKGGhMCa9JEkCCjbR2UMcTmB69:5K6BU1VoDiGSkAVpwyu5YgxnmuiGwNyVW5RLtAnDkyS3tqAL5Jz")
+run("litecoin", "LaZwBFXjGyWKx9tjKSE3UDoMdEqdk8ngjw:6vPuw93L88jKDYPgToh3L5jxjPGk9BRXGkpWbMoFURkfacZbYRS")
+run("ethereum", "0xc31b85901fceb7f4d56d1dc55ba5b3fb57f300a7:a7c96262c21db9a06fd49e307d694fd95f624569f9b35bb3ffacd880440f9787")
